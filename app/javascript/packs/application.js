043a555ff62df4a10bf1cf0d6fd5d9977e717f73
ctw <ctw.3@163.com>
2018-09-14 12:23:40 +0800

    用Vue.js完成curd

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,38 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// import Vue from 'vue'
+// import App from '../lists.vue'
 //
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+// document.addEventListener('DOMContentLoaded', () => {
+//   const el = document.body.appendChild(document.createElement('list'))
+//   const app = new Vue({
+//     el,
+//     render: h => h(App)
+//   })
+//
+//   console.log(app)
+// })
 
-console.log('Hello World from Webpacker')
+//
+// If the using turbolinks, install 'vue-turbolinks':
+//
+// yarn add 'vue-turbolinks'
+//
+// Then uncomment the code block below:
+//
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../lists.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+
+  var app = new Vue({
+    el: '#list',
+    components: { App }
+  })
+
+  console.log(app)
+})
