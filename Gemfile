1b302be545c3fb3b753092836cf95b7c27b18a31
ctw <ctw.3@163.com>
2018-11-02 22:16:57 +0800

    add gem

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ group :development, :test do
   gem 'capistrano'
   gem 'capistrano-rails'
   gem 'capistrano-passenger'
-
+  gem 'capistrano-rvm'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@github.com:chentianwei411/vue-lists.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/vue-todolist"
+set :deploy_to, "/home/chentianwei/vue-todolist"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
